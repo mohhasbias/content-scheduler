eb3bce2ec8089388618e000f0dbbc7eb53810fb2
Moh Hasbi Assidiqi <mohhasbias@gmail.com>
2017-03-10 21:05:42 +0700

    return json

--- index.php
+++ index.php
@@ -5,7 +5,25 @@ require_once __DIR__.'/vendor/autoload.php';
 $app = new Silex\Application();
 
 $app->get('/', function() use($app) {
-  return 'Hello';
+  return $app->json(
+    array(
+      array(
+        'url' => 'http://madingkampus.com/pamflet/17-03-02-madingkampus-kpjlhlefqa-.jpeg',
+        'type' => 'image/jpeg',
+        'duration' => 1
+      ),
+      array(
+        'url' => 'http://madingkampus.com/pamflet/17-02-21-madingkampus-alkacgapjf-.PNG',
+        'type' => 'image/png',
+        'duration' => 1
+      ),
+      array(
+        'url' => 'http://madingkampus.com/pamflet/17-01-17-madingkampus-kqdeqhgnne-.png',
+        'type' => 'image/png',
+        'duration' => 1
+      )
+    )
+  );
 });
 
 $app->run();
